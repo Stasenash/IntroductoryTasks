cf0fdb2f5e7b109624c896a99c13b246c74ffd3c
Stasenash <staslisenash@gmail.com>
2020-03-15 00:59:27 +0500

    Added some tests

--- Task2/src/com/example/task2/Task2Main.java
+++ Task2/src/com/example/task2/Task2Main.java
@@ -4,16 +4,15 @@ import com.sun.istack.internal.Nullable;
 
 public class Task2Main {
     public static void main(String[] args) {
-
-        decryptData(new int[]{5,100,20,66,16},50,1,3);
-        decryptData(new int[]{5,100,20,66,16},50,1,7); //не топь, еще что ли проверок надобавлять
     }
 
-    public static int[] decryptData(int[] price, int discount, int offset,int readLength){
-        if (price == null || price.length == 0 || discount < 1 || discount > 99 || offset < 0 || readLength < 1) throw  new IllegalArgumentException("Wrong data");
+    public static @Nullable int[] decryptData(int[] price, int discount, int offset,int readLength){
+        if (price == null || price.length == 0 || discount < 1 || discount > 99 || offset < 0 || readLength < 1)
+            throw  new IllegalArgumentException("Wrong data"); //проверки на корректность данных
         int max = readLength + offset;
         int[] result = new int[readLength];
-        for (int i = offset; i < max && i < price.length; i++){
+        for (int i = offset; i < max && i < price.length; i++){ //даже если readLength больше длины массива,
+            // корректо обрабатываем пока не кончится массив
             result[i - offset] = (int) Math.floor(price[i] * (100 - discount) * 0.01);
         }
         return result;
--- Task2/src/com/example/task2/Tests.java
+++ Task2/src/com/example/task2/Tests.java
@@ -6,43 +6,22 @@ import org.junit.Test;
 public class Tests {
     @Test
     public void testCanDoSimpleTask(){
-        Assert.assertArrayEquals("Проверка массива из \"дано\"",new int[] {50, 10, 33},Task2Main.decryptData(new int[]{5,100,20,66,16},50,1,3));
+        Assert.assertArrayEquals("Checking array from source data",new int[] {50, 10, 33},Task2Main.decryptData(new int[]{5,100,20,66,16},50,1,3));
+        Assert.assertArrayEquals(new int[] {50,10,33,8,0,0,0}, Task2Main.decryptData(new int[]{5,100,20,66,16},50,1,7));
     }
 
-
     @Test
-    public void testWrongDiscount(){
-        try {
-            Task2Main.decryptData(new int[]{10,12,35},100,0,2);
-            Assert.fail("Fail because exception was not thrown. Wrong discount");
-        }catch (IllegalArgumentException e) {
-            //true;
-        }
+    public void testWrongData(){
+        doExceptionsCheck(new int[]{10,12,35},100,0,2);
+        doExceptionsCheck(new int[]{10,12,35},99,0,-1);
+        doExceptionsCheck(new int[]{10,12,35},99,-20,5);
+        doExceptionsCheck(new int[]{},90,0,5);
+        doExceptionsCheck(null,50,0,3);
+    }
 
+    private void doExceptionsCheck(int[] price, int discount, int offset, int readLength ){
         try {
-            Task2Main.decryptData(new int[]{10,12,35},99,0,-1);
-            Assert.fail("Fail because exception was not thrown. Wrong readLength");
-        }catch (IllegalArgumentException e){
-            //true;
-        }
-
-
-        try {
-            Task2Main.decryptData(new int[]{10,12,35},99,-20,5);
-            Assert.fail("Fail because exception was not thrown. Wrong offset");
-        }catch (IllegalArgumentException e){
-            //true;
-        }
-
-        try {
-            Task2Main.decryptData(new int[]{},90,0,5);
-            Assert.fail("Fail because exception was not thrown. Array length = 0");
-        }catch (IllegalArgumentException e){
-            //true;
-        }
-
-        try {
-            Task2Main.decryptData(null,50,0,3);
+            Task2Main.decryptData(price, discount, offset, readLength);
             Assert.fail("Fail because exception was not thrown. Array is null");
         }catch (IllegalArgumentException e){
             //true;
