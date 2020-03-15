dc7a3fa14dc46fa23a1eed0205b33ed15e9f217e
Stasenash <staslisenash@gmail.com>
2020-03-15 12:04:29 +0500

    added method documentation

--- Task2/src/com/example/task2/Task2Main.java
+++ Task2/src/com/example/task2/Task2Main.java
@@ -3,8 +3,19 @@ package com.example.task2;
 import com.sun.istack.internal.Nullable;
 
 public class Task2Main {
+
     public static void main(String[] args) {
     }
+
+    /**
+     * Метод "скидка". Применяет скидку discount к цене price, начиная с позиции offset
+     * на количество позиций readLength. Новые цены округляем “вниз”,
+     * до меньшего целого числа.  * @param price - исходные цены.
+     * @param discount - % скидки, от 1 до 99.
+     * @param offset - номер позиции, с которой нужно применить скидку.
+     * @param readLength - количество позиций, к которым нужно применить скидку.
+     @return - массив новых цен.
+     */
 
     public static @Nullable int[] decryptData(int[] price, int discount, int offset,int readLength){
         if (price == null || price.length == 0 || discount < 1 || discount > 99 || offset < 0 || readLength < 1)
